42c70faf41afc3928b48db8ce770b1e05a0e3f92
Diego Cataldo <cataldo.diego@gmail.com>
2025-10-10 15:10:23 -0300

    code claening

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,7 @@ function App() {
         // Background Media Options
         backgroundOpacity={40}
         backgroundSize="cover"
-        // Blend Mode - uncomment to test different effects:
-        // backgroundBlendMode="multiply"     // Darker, multiplies colors
-        // backgroundBlendMode="screen"       // Lighter, inverted multiply
-        // backgroundBlendMode="overlay"      // Combines multiply and screen
         backgroundBlendMode="luminosity"   // Uses luminosity of video with color of background
-        // backgroundBlendMode="color"        // Uses color of background with luminosity of video
-        // backgroundBlendMode="soft-light"   // Subtle lighting effect
         // Background Image or Video
         mediaType="video"
         backgroundMedia="/videos/stock-footage-a-newborn-baby-whale-seeks-protection-from-its-mother-by-swimming-close-to-her-side-drone-view.webm"
